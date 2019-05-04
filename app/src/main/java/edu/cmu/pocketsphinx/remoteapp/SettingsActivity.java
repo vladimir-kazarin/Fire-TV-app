8653444c56886b6965fc7b4680a30c14f6d296fa
vladmir.kazarin <vladimir.kazarin@csquaredsystems.com>
2019-05-04 16:29:01 -0400

    Adding button icons

---
+++ app/src/main/java/edu/cmu/pocketsphinx/remoteapp/SettingsActivity.java
@@ -0,0 +1,26 @@
+package edu.cmu.pocketsphinx.remoteapp;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        // my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
