0adbf2e1be13fdf7a0836d1f7ab439dbb31a0dc7
vladmir.kazarin <vladimir.kazarin@csquaredsystems.com>
2019-05-04 16:08:06 -0400

    Adding new front-end

--- app/src/main/java/edu/cmu/pocketsphinx/remoteapp/MainActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/remoteapp/MainActivity.java
@@ -1,10 +1,11 @@
 package edu.cmu.pocketsphinx.remoteapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,16 +22,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "SettingsActivity" item, show the app settings UI...
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
 
     public void onConnectClick(View v) {
         String ipAddress = ((TextView) this.findViewById(R.id.ipAddress)).getText().toString();
@@ -116,5 +133,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         return output;
     }
-
 }
