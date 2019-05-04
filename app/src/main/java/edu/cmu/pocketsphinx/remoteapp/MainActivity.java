ee53e1cb35f259241c89031522f2da0ae28182e9
vladmir.kazarin <vladimir.kazarin@csquaredsystems.com>
2019-05-04 16:53:12 -0400

    Adding new front end version 3 with spinner

--- app/src/main/java/edu/cmu/pocketsphinx/remoteapp/MainActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/remoteapp/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +24,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.search_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.search_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
 
     @Override
